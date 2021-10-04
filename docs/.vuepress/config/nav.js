210ef3e9717d148a21d3d07b457486f1c3770ee3
icpove <gaoajia@qq.com>
2021-10-04 14:10:00 +0800

    init

---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,29 @@
+module.exports = [
+  { text: '首页', link: '/' },
+  {
+    text: '指南', link: '/pages/a2f161/', items: [
+      { text: '第一次使用', link: '/pages/52d5c3/' },
+      { text: '手机使用', link: '/pages/a2f161/' },
+      { text: 'pad使用', link: '/pages/793dcb/' },
+      { text: '电脑使用', link: '/pages/2f674a/' },
+      { text: '特殊设备', link: '/pages/33d574/' },
+      {
+        text: '相关文章', items: [
+          { text: 'WiFi接入认证过程简析', link: '/pages/dd027d/' },
+          { text: '待更', link: '/pages/2b8e22/' },
+          { text: '待更', link: '/pages/0fc1d2/' },
+        ]
+      }
+    ]
+  },
+  {
+    text: '故障', link: '/pages/a20ce8/', items: [
+      { text: '待更', link: '/pages/a20ce8/' },
+      { text: '待更', link: '/pages/f14bdb/' },
+    ]
+  },
+  { text: '待更', link: '/pages/db78e2/' },
+  { text: '待更', link: '/pages/5d571c/' },
+  { text: '问答', link: '/pages/9cc27d/' },
+  { text: '支持', link: '/pages/1b12ed/' },
+  { text: '关于', link: '/pages/5dfce5/' }]
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+---
+home: true
+heroImage: https://image.gaoajia.com/2021/09/25/135503571a730.jpg
+heroText: GiWiFi使用文档
+tagline: 🚀一个非官方GWiFi校园网使用帮助文档
+actionText: 快速开始 →
+actionLink: /pages/52d5c3/
+bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式       提示：如发现文本颜色不适应你的背景时可以到palette.styl修改$bannerTextColor变量
+
+features: # 可选的
+  - title: 洛理专属
+    details: 本文档适合洛理学子参阅，内容仅针对使用校园网过程中出现的各种问题，给予相应的处理方式
+  - title: 站长寄语
+    details: 站长缺乏艺术细胞，所以这里没有华丽的词藻，只愿用拙劣的文笔，撰写内心最真诚的想法和建议
+  - title: 沉浸阅读
+    details: 专为阅读设计的UI，配合多种颜色模式、可关闭的侧边栏和导航栏，带给你一种沉浸式阅读体验
+
+# 文章列表显示方式: detailed 默认，显示详细版文章列表（包括作者、分类、标签、摘要、分页等）| simple => 显示简约版文章列表（仅标题和日期）| none 不显示文章列表
+postList: none
+---
+
